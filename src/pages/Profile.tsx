aad4e002f16bfbfd0a335e191441848bd17c9573
Johan <johanvilla64@gmail.com>
2025-03-15 17:57:49 -0500

    WIP :construction: add Profile page placeholder with authentication protection

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,6 @@
+//TODO: Create a Profile page that displays the user's profile information. This page should be protected and only accessible to authenticated users.
+const Profile = () => {
+  return <div>Profile placeholder</div>;
+};
+
+export default Profile;
