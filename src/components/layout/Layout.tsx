8487c441ce224908852e36706f1db72d9054664d
Johan <johanvilla64@gmail.com>
2025-03-18 11:39:45 -0500

    feat :sparkles: add Layout component to structure page with Navbar and Sidebar

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Sidebar from "./SideBar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <Navbar />
+        <main className="flex-1 p-4">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
