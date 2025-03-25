fd48f4ee314f02873f0ceddad3571f6516a65d3d
Johan <johanvilla64@gmail.com>
2025-03-25 10:31:38 -0500

    fix: :art: standardize import statement quotes and improve user information formatting in Users component

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,13 +1,11 @@
-import UserList from "../../components/ui/UserList";
+import UserList from '../../components/ui/UserList';
 
 const Users = () => {
   return (
     <>
       <header className="mb-8">
         <h1 className="text-3xl font-bold">Usuarios</h1>
-        <p className="text-muted-foreground dark:text-muted-dark">
-          Información de los usuarios
-        </p>
+        <p className="text-muted-foreground">Información de los usuarios</p>
       </header>
       <section>
         <UserList />
