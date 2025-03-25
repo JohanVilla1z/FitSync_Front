4254a1a5a40a6d9341adf6350ef80a9857d6101d
Johan <johanvilla64@gmail.com>
2025-03-25 10:31:28 -0500

    feat: :sparkles: add useEquipmentStore to central store exports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
 export { useAuthStore } from './authStore';
+export { useEquipmentStore } from './useEquipmentStore';
 export { useSidebarStore } from './useSideBarStore';
 export { useThemeStore } from './useThemeStore';
 export { useUsersStore } from './useUsersStore';
+
