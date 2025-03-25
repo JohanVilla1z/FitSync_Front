ba20d327fa093bb13ce9e39f17d223f5ca2b4983
Johan <johanvilla64@gmail.com>
2025-03-25 12:40:18 -0500

    refactor: :recycle: update Trainer interface to include active status and user IDs

--- src/constants/Trainer.ts
+++ src/constants/Trainer.ts
@@ -1,9 +1,8 @@
-import { User } from './User';
-
 export interface Trainer {
   id?: number; // Opcional, ya que no se envía al crear un nuevo entrenador
   name: string;
   email: string;
   password: string;
-  users?: User[]; // Lista de usuarios asignados al entrenador (opcional)
+  active: boolean;
+  userIds: number[]; // Lista de usuarios asignados al entrenador (opcional)
 }
