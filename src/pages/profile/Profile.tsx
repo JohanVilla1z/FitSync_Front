02dab162c6e3a7b472b5552368f4c92295aa750a
Johan <johanvilla64@gmail.com>
2025-03-25 11:09:31 -0500

    refactor: :art: refactor Profile component for improved structure and accessibility

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
-import { useAuthStore } from '../../store';
-import { useUserProfileStore } from '../../store/useUserProfileStore';
-import { getAvatarByRole } from '../../utils';
-import { getActiveSinceMessage, getGreeting } from '../../utils/timeUtils';
-import { getImcDiagnose } from '../../utils/getImcDiagnose'; // Importar la función
+import { useAuthStore, useUserProfileStore } from '../../store';
+import {
+  getActiveSinceMessage,
+  getAvatarByRole,
+  getGreeting,
+  getImcDiagnose,
+} from '../../utils';
 
 const Profile = () => {
   const { user } = useAuthStore();
@@ -27,7 +29,6 @@ const Profile = () => {
     );
   }
 
-  // Generar el diagnóstico del IMC
   const imcDiagnosis = profile.currentIMC
     ? getImcDiagnose(profile.currentIMC)
     : 'No se pudo calcular el diagnóstico del IMC.';
@@ -38,46 +39,56 @@ const Profile = () => {
       role="region"
       aria-labelledby="profile-header"
     >
-        {/* Encabezado */}
-        <div className="flex flex-col items-center mb-6">
-          <img
-            src={getAvatarByRole(user?.role)}
-            alt="Avatar"
-            className="w-56 h-56 rounded-full mb-4 bg-gray-200 dark:bg-gray-700"
-          />
-          <h1 className="text-3xl font-bold text-foreground dark:text-foreground-dark">
-            {getGreeting()}, {profile.name}!
-          </h1>
-          <p className="text-muted dark:text-muted-dark">{profile.email}</p>
-        </div>
+      {/* Encabezado */}
+      <header className="flex flex-col items-center mb-6">
+        <img
+          src={getAvatarByRole(user?.role)}
+          alt={`Avatar del usuario con rol ${user?.role}`}
+          className="w-56 h-56 rounded-full mb-4 bg-gray-200 dark:bg-white"
+        />
+        <h1
+          id="profile-header"
+          className="text-3xl font-bold text-foreground dark:text-foreground-dark"
+        >
+          {getGreeting()}, {profile.name}!
+        </h1>
+        <p className="text-muted dark:text-muted-dark">{profile.email}</p>
+      </header>
 
-        {/* Información Básica */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-6">
-          <div>
+      {/* Información Básica */}
+      <section aria-labelledby="basic-info-header" className="mb-6">
+        <h2
+          id="basic-info-header"
+          className="text-2xl font-semibold text-foreground dark:text-foreground-dark"
+        >
+          Información Básica
+        </h2>
+        <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <li>
             <p className="text-sm font-medium text-muted dark:text-muted-dark">
               Teléfono
             </p>
             <p className="text-lg text-foreground dark:text-foreground-dark">
               {profile.phone || 'No disponible'}
             </p>
-          </div>
-          <div>
+          </li>
+          <li>
             <p className="text-sm font-medium text-muted dark:text-muted-dark">
               Altura
             </p>
             <p className="text-lg text-foreground dark:text-foreground-dark">
               {profile.height ? `${profile.height} m` : 'No disponible'}
             </p>
-          </div>
-          <div>
+          </li>
+          <li>
             <p className="text-sm font-medium text-muted dark:text-muted-dark">
               Peso
             </p>
             <p className="text-lg text-foreground dark:text-foreground-dark">
               {profile.weight ? `${profile.weight} kg` : 'No disponible'}
             </p>
-          </div>
-          <div>
+          </li>
+          <li>
             <p className="text-sm font-medium text-muted dark:text-muted-dark">
               IMC Actual
             </p>
@@ -86,30 +97,31 @@ const Profile = () => {
                 ? profile.currentIMC.toFixed(2)
                 : 'No disponible'}
             </p>
-          </div>
-        </div>
+          </li>
+        </ul>
+      </section>
 
-        {/* Diagnóstico del IMC */}
-        <div className="mb-6">
-          <p className="text-sm font-medium text-muted dark:text-muted-dark">
-            Diagnóstico del IMC
-          </p>
-          <p className="text-lg text-foreground dark:text-foreground-dark">
-            {imcDiagnosis}
-          </p>
-        </div>
+      {/* Diagnóstico del IMC */}
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold text-foreground dark:text-foreground-dark">
+          Diagnóstico del IMC
+        </h2>
+        <p className="text-lg text-foreground dark:text-foreground-dark">
+          {imcDiagnosis}
+        </p>
+      </section>
 
-        {/* Fecha de Registro */}
-        <div className="mb-6">
-          <p className="text-sm font-medium text-muted dark:text-muted-dark">
-            Fecha de Registro
-          </p>
-          <p className="text-lg text-foreground dark:text-foreground-dark">
-            {getActiveSinceMessage(profile.registerDate)} ({profile.registerDate})
-          </p>
-        </div>
+      {/* Fecha de Registro */}
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold text-foreground dark:text-foreground-dark">
+          Fecha de Registro
+        </h2>
+        <p className="text-lg text-foreground dark:text-foreground-dark">
+          {getActiveSinceMessage(profile.registerDate)} ({profile.registerDate})
+        </p>
+      </section>
 
-        {/* Entrenador */}
+      {/* Entrenador */}
       <section className="mb-6">
         <h2 className="text-2xl font-semibold text-foreground dark:text-foreground-dark">
           Entrenador
@@ -125,19 +137,19 @@ const Profile = () => {
               </p>
             </li>
             <li>
-          <p className="text-sm font-medium text-muted dark:text-muted-dark">
+              <p className="text-sm font-medium text-muted dark:text-muted-dark">
                 Correo Electrónico
-          </p>
-            <p className="text-lg text-foreground dark:text-foreground-dark">
+              </p>
+              <p className="text-lg text-foreground dark:text-foreground-dark">
                 {profile.trainerEmail || 'No disponible'}
-            </p>
+              </p>
             </li>
           </ul>
-          ) : (
-            <p className="text-lg text-red-500">
+        ) : (
+          <p className="text-lg text-red-500">
             Aún no tienes un entrenador asignado.
-            </p>
-          )}
+          </p>
+        )}
       </section>
     </section>
   );
