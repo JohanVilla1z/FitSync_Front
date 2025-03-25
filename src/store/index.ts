abe33ee0413071c4dddb9b5ee22dcfdcd45d0d1d
Johan <johanvilla64@gmail.com>
2025-03-25 11:07:51 -0500

    feat: :sparkles: add useUserProfileStore to central store exports

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,5 +2,5 @@ export { useAuthStore } from './authStore';
 export { useEquipmentStore } from './useEquipmentStore';
 export { useSidebarStore } from './useSideBarStore';
 export { useThemeStore } from './useThemeStore';
+export { useUserProfileStore } from './useUserProfileStore';
 export { useUsersStore } from './useUsersStore';
-
