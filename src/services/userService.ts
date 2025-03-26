e6d41fe001508f53b008f14bbc16556211f09a21
Johan <johanvilla64@gmail.com>
2025-03-26 16:49:46 -0500

    feat: :sparkles: add registerGymEntry function to log gym entries for authenticated users

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -7,11 +7,30 @@ import { EntryLog } from '../constants';
  */
 export const getUserEntryLogs = async (): Promise<EntryLog[]> => {
   try {
-    const response = await axiosInstance.get<EntryLog[]>('/entry-logs/user-history');
+    const response = await axiosInstance.get<EntryLog[]>(
+      '/entry-logs/user-history'
+    );
     return response.data;
   } catch (error: any) {
     console.error(
       'Error al obtener el historial de entradas:',
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
+
+/**
+ * Registra una nueva entrada al gimnasio para el usuario autenticado.
+ * @returns Los datos de la entrada registrada.
+ */
+export const registerGymEntry = async (): Promise<EntryLog> => {
+  try {
+    const response = await axiosInstance.post<EntryLog>('/entry-logs');
+    return response.data;
+  } catch (error: any) {
+    console.error(
+      'Error al registrar entrada al gimnasio:',
       error.response?.data || error.message
     );
     throw error;
