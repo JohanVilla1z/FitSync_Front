f11346ffd1391d3e509f211511d4c428ec03a8ff
Johan <johanvilla64@gmail.com>
2025-03-26 18:39:07 -0500

    fix: :lipstick: refactor Sidebar component for improved readability and consistency

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,17 +1,17 @@
 import {
+  Dumbbell,
   Home,
   LogOut,
   Menu,
   Users,
-  Dumbbell,
   UserSearch,
   X,
-} from "lucide-react";
-import { NavLink } from "react-router-dom";
-import { Role } from "../../constants/RolEnum";
-import { cn } from "../../lib/utils";
-import { useAuthStore } from "../../store/authStore";
-import { useSidebarStore } from "../../store/useSideBarStore";
+} from 'lucide-react';
+import { NavLink } from 'react-router-dom';
+import { Role } from '../../constants/RolEnum';
+import { cn } from '../../lib/utils';
+import { useAuthStore } from '../../store/authStore';
+import { useSidebarStore } from '../../store/useSideBarStore';
 
 const Sidebar = () => {
   const { isCollapsed, toggleSidebar } = useSidebarStore();
@@ -22,18 +22,18 @@ const Sidebar = () => {
   }
 
   const links = [
-    { to: "/dashboard", label: "Dashboard", icon: Home },
-    { to: "/trainers", label: "Entrenadores", icon: Users },
-    { to: "/equipment", label: "Equipos", icon: Dumbbell },
-    { to: "/users", label: "Usuarios", icon: UserSearch },
+    { to: '/dashboard', label: 'Dashboard', icon: Home },
+    { to: '/trainers', label: 'Entrenadores', icon: Users },
+    { to: '/equipment', label: 'Equipos', icon: Dumbbell },
+    { to: '/users', label: 'Usuarios', icon: UserSearch },
   ];
 
   return (
     <aside
       className={cn(
-        "flex flex-col top-0 left-0 h-full bg-gray-900 text-white transition-all duration-300",
-        "dark:bg-background-dark dark:text-gray-200", // Clases para tema oscuro
-        isCollapsed ? "w-14" : "w-64"
+        'flex flex-col top-0 left-0 min-h-screen bg-gray-900 text-white transition-all duration-300',
+        'dark:bg-background-dark dark:text-gray-200', // Clases para tema oscuro
+        isCollapsed ? 'w-14' : 'w-64'
       )}
     >
       <nav className="mt-4 ">
@@ -48,8 +48,8 @@ const Sidebar = () => {
             to={to}
             className={({ isActive }) =>
               cn(
-                "flex items-center p-4 space-x-3 transition",
-                isActive ? "bg-gray-700" : "hover:bg-gray-800"
+                'flex items-center p-4 space-x-3 transition',
+                isActive ? 'bg-gray-700' : 'hover:bg-gray-800'
               )
             }
           >
