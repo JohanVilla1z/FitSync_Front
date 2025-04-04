f33dbfd66c05f27a5e757ef69d859ba56be480d3
Johan <johanvilla64@gmail.com>
2025-04-03 20:50:28 -0500

    feat: :sparkles: add useTrainersStore export to central store for trainer management

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,5 +3,6 @@ export { useEntryLogStore } from './useEntryLogStore';
 export { useEquipmentStore } from './useEquipmentStore';
 export { useSidebarStore } from './useSideBarStore';
 export { useThemeStore } from './useThemeStore';
+export { useTrainersStore } from './useTrainersStore';
 export { useUserProfileStore } from './useUserProfileStore';
 export { useUsersStore } from './useUsersStore';
